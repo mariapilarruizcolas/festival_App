27eaf63e9aaaa266509d55687df24e44b04796d2
MariaPilarRuizColas <mariapilar.ruizcolas@neuf.fr>
2021-11-25 22:03:49 +0100

    Css form improved

--- festival_app/src/App.js
+++ festival_app/src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import './App.css';
-import NewUserForm from './Components/NewUserForm';
-import ConnectionPage from './Components/ConnectionPage';
+//import NewUserForm from './Components/NewUserForm';
+//simport ConnectionPage from './Components/ConnectionPage';
 import { Routes, Route } from 'react-router-dom'
 import Accueil from '../src/Screen/Accueil.jsx'
+import Login from './Screen/Login';
 
 function App() {
   return (
     <div className="App">
-     <ConnectionPage />
-
-     
+         
      <Routes>
       <Route exact path="/" element={<Accueil/>} />
+      <Route  path="/Login" element={<Login/>} />
      </Routes>
     </div>
   );
--- festival_app/src/Components/NewUserForm.js
+++ festival_app/src/Components/NewUserForm.js
@@ -1,38 +1,39 @@
 import React from "react";
+import "./NewUserForm.css";
 
 function NewUserForm() {
   return (
-    
-  <div>
-      <h1>Become Festival User</h1>
+    <section className="container-card">
+  <div className="cardForm">
+      <h1 className="titleForm">Become Festival User</h1>
   <form className="classForm" methode="post">
-        <div>
-        <label for="userName">User name</label>
-        <input type="text" name="userName" id="userName" required/>
+        <div classname="itemsForm">
+        <label  for="userName">User name</label>
+        <input className="inputForm" type="text" name="userName" id="userName" required/>
         </div>
-        <div>
+        <div className="itemsForm">
         <label for="Firstname" name="firstname" id="firstname">Firstname</label>
-        <input type="text" name="firstname" id="firstname" required/>
+        <input className="inputForm"  type="text" name="firstname" id="firstname" required/>
         </div>
-        <div>
+        <div className="itemsForm">
 
         <label for="lastname">Lastname</label>
-        <input type="text" name="lastname" id="lastname" required/>
+        <input className="inputForm" type="text" name="lastname" id="lastname" required/>
         </div>
-      <div>
-        <label for="email">Your mail adress</label>
-        <input type="text" id="email" name="email" required/>
+      <div className="itemsForm">
+        <label for="email">Your mail </label>
+        <input className="inputForm"  type="text" id="email" name="email" required/>
       </div>
-      <div>
+      <div className="radioForm">
         
-      <input type="radio" name="sex" value="woman" id="sex" required/> <label for="sex">Woman</label>
-      <input type="radio" name="sex" value="man" id="sex" required/> <label for="sex">Man</label>
+      <input className="inputForm"  type="radio" name="sex" value="woman" id="sex" required/> <label for="sex">Woman</label>
+      <input className="inputForm"  type="radio" name="sex" value="man" id="sex" required/> <label for="sex">Man</label>
       </div>
-      <div>
+      <div classname="itemsForm">
         <label for="userMusic" id="userMusic" name="userMusic">
-          Choise your favourite music
+          Choise your music
         </label>
-        <select id="userMusic" name="userMusic" required>
+        <select className="inputForm" id="userMusic" name="userMusic" required>
           <option value="Hip-Hop">Hip-Hop</option>
           <option value="Hard-Rock">Hard-Rock</option>
           <option value="Techno">Techno</option>
@@ -41,22 +42,23 @@ function NewUserForm() {
           <option value="Reggae">Reggae</option>
         </select>
       </div>
-      <div>
+      <div classname="itemsForm">
       <label for="country">Your country</label>
-        <input type="text" id="country" name="country" required/>
+        <input className="inputForm"  type="text" id="country" name="country" required/>
       </div>
-      <div>
-        <label for="pass">Your password :</label>
-        <input type="password" name="password" id="password" required/>
+      <div classname="itemsForm">
+        <label for="pass">Your password </label>
+        <input className="inputForm"  type="password" name="password" id="password" required/>
       </div>
-      <div>
+      <div classname="itemsForm">
 
-        <button type="submit">Create User</button>
+        <button className="buttonForm" type="submit">Create User</button>
+        <button className="buttonForm" type="submit">New here</button>
       </div>
       </form>
     
   </div>
-  
+  </section>
   );
 }
 
